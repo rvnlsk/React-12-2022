d4ca2e5d6babfd3f346c533bb0d6d4f3d8b6890e
rvnlsk <119679071+rvnlsk@users.noreply.github.com>
2022-12-05 15:57:44 +0200

    uus reacti projekt

---
+++ veebipood/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://www.koeratoit.ee/media/wysiwyg/6BD96F36-C806-4BA5-ADF3-2B43D720A741_002_.jpg" alt="" />
+      <button className="nupp">Nupp</button>
+      
+    </div>
+  );
+}
+export default App;
