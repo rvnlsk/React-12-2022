d6abb913323c4ed8df36babef820bc4a57bc8f19
rvnlsk <119679071+rvnlsk@users.noreply.github.com>
2022-12-06 23:47:55 +0200

    kodutoo1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://i0.wp.com/www.laketobias.com/wp-content/uploads/2021/03/capybara-sq.jpg?fit=1080%2C1080&ssl=1" alt="" />
+      <button className="nupp">nupuke</button>
+      <h4 className="tekst">See on kapibaara</h4>
+    </div>
+  );
+}
+
+export default App;
