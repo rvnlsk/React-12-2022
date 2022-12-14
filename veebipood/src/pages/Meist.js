f771b7f45e9e83559283a64f64aa37e7d2c67a08
rvnlsk <119679071+rvnlsk@users.noreply.github.com>
2022-12-14 16:16:59 +0200

    array, string

--- veebipood/src/pages/Meist.js
+++ veebipood/src/pages/Meist.js
@@ -1,12 +1,20 @@
-
+import { useState } from 'react'
 
 function Meist() {
+  const [naitaEmaili, uuendaNaitaEmaili] = useState(false);
+  const [telefon, uuendaTelefon] = useState(localStorage.getItem("meie_telefon"));
+
   return (
     <div>
-      <div>Meie e-mail: {localStorage.getItem("meie_email")}</div>
-      <div>Meie telefoninumber: {localStorage.getItem("meie_telefon")}</div>
-      
-    </div>
+      <div>Meie e-mail: 
+        {naitaEmaili === true && localStorage.getItem("meie_email")} 
+        {naitaEmaili === false && <button onClick={() => uuendaNaitaEmaili(true)}>Naita emaili</button> }
+        </div>
+      <div>Meie telefoninumber: 
+        {telefon} 
+       {telefon.startsWith("+372") === false && <button onClick={() => uuendaTelefon("+372" + telefon)}>Pane +372 ette</button>}
+        </div>
+      </div>
   )
 }
 
--- veebipood/src/pages/Poed.js
+++ veebipood/src/pages/Poed.js
@@ -1,9 +1,110 @@
-
+import { useState } from 'react';
 
 function Poed() {
+  const [poed, uuendaPoed] = useState(["Kristiine", "Kesklinnake", "Marja", "Telliskivi", "Kirsilinn", "Maalinn"])
+
+  const paneTagasi = () => {
+    uuendaPoed(["Kristiine", "Kesklinnake", "Marja", "Telliskivi", "Kirsilinn", "Maalinn"])
+  }
+
+  const sorteeriAZ = () => {
+  poed.sort();
+  console.log(poed);
+  uuendaPoed(poed.slice());
+
+  }
+
+  const sorteeriZA = () => {
+    poed.sort((a, b) => b.localeCompare(a));
+    uuendaPoed(poed.slice());
+
+ }
+
+ const sorteeriTahtedeJarjekorras = () => {
+  poed.sort((a, b) => a.length - b.length);
+  uuendaPoed(poed.slice());
+
+ }
+
+ const sorteeriTahedVastupidi = () => {
+  poed.sort((a, b) => b.length - a.length);
+  uuendaPoed(poed.slice());
+
+ }
+
+ const filtreeriKivi = () => {
+  const tulem = poed.filter(element => element.endsWith("kivi") === true);
+  uuendaPoed(tulem);
+ }
+
+ const filtreeriLinn = () => {
+  const tulem = poed.filter(element => element.includes("linn") === true);
+  uuendaPoed(tulem);
+ }
+
+ const filtreeriTaheArv7 = () => {
+  const tulem = poed.filter(element => element.length === 7);
+  uuendaPoed(tulem);
+ }
+
+ const filtreeriTaheArvVaiksemKui7 = () => {
+  const tulem = poed.filter(element => element.length < 7);
+  uuendaPoed(tulem);
+ }
+
+ const filtreeriKellelKolmasS= () => {
+  const tulem = poed.filter(element => element.charAt(2) === "s");
+  uuendaPoed(tulem);
+ }
+
+ const muudaKoikiSuureks = () => {
+  const tulem = poed.map(element => element.toUpperCase());
+  uuendaPoed(tulem);
+
+  }
+
+  const muudaKoigilePikkuseNrLoppu = () => {
+    const tulem = poed.map(element => element + element.length);
+    uuendaPoed(tulem);
+  }
+
+  const muudaKoigileIAsemelO = () => {
+    const tulem = poed.map(element => element.replaceAll("i", "o"));
+    uuendaPoed(tulem);
+  }
+
+
   return (
-    <div>Poed</div>
+    <div>
+      <button onClick={paneTagasi}>Pane koik tagasi</button>
+      <button onClick={sorteeriAZ}>Sorteeri A-Z</button>
+      <button onClick={sorteeriZA}>Sorteeri Z-A</button>
+      <button onClick={sorteeriTahtedeJarjekorras}>Sorteeri tahtede jargi</button>
+      <button onClick={sorteeriTahedVastupidi}>Sorteeri tahtede jargi kahanevalt</button>
+      <button onClick={filtreeriKivi}>Jata alles kivi lopuga sonad</button>
+      <button onClick={filtreeriLinn}>Jata alles "linn" sisaldavad</button>
+      <button onClick={filtreeriTaheArv7}>Jata alles kellel tahemarke 7</button>
+      <button onClick={filtreeriTaheArvVaiksemKui7}>Jata alles kellel vaiksem kui 7</button>
+      <button onClick={filtreeriKellelKolmasS}>Jata alles kellel kolmas taht S</button>
+      <button onClick={muudaKoikiSuureks}>Muuda koik suurteks tahtedeks</button>
+      <button onClick={muudaKoigilePikkuseNrLoppu}>Pikkuse nr loppu</button>
+      <button onClick={muudaKoigileIAsemelO}>I asemel O</button>
+
+      <div>{poed.lenght}</div>
+      {poed.map((yksPood,index) => <div key={index}>{yksPood}</div>)}
+      <div>-----------------</div>
+      <div>Kristiine</div>
+      <div>Kesklinnake</div>
+      <div>Marja</div>
+      <div>Telliskivi</div>
+      <div>----------------</div>
+      <div>BMW</div>
+      <div>Audi</div>
+      <div>Mercedes</div>
+      <div>{["BMW", "Audi", "Mercedes"].lenght}</div>
+      {["BMW", "Audi", "Mercedes"].map((auto,i) => <div key={i}>{auto}</div>)}
+    </div>
   )
 }
 
-export default Poed
+export default Poed 
